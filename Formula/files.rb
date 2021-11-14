f00761c129c329c206fc6e9fcbf79db9ba519cb2
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2021-11-15 04:42:04 +0900

    Add files

---
+++ Formula/files.rb
@@ -0,0 +1,13 @@
+class Files < Formula
+  desc 'Fast file find'
+  homepage 'https://github.com/mattn/files'
+  version '0.3.3'
+  license 'MIT'
+
+  url "https://github.com/mattn/files/releases/download/v#{version}/files_v#{version}_darwin_amd64.zip"
+  sha256 '187594f54037e6261c116e04c57a1c8d9f113ded8d3b26e9b61440b6a53c93e5'
+
+  def install
+    bin.install 'files'
+  end
+end
