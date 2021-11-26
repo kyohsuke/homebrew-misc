434642af829c1a67cae6f93ca9741dddf4921e67
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2021-11-27 04:18:49 +0900

    Tweaks

--- Formula/files.rb
+++ Formula/files.rb
@@ -4,7 +4,7 @@ class Files < Formula
   version '0.3.3'
   license 'MIT'
 
-  url "https://github.com/mattn/files/releases/download/v#{version}/files_v#{version}_darwin_amd64.zip"
+  url "#{homepage}/releases/download/v#{version}/files_v#{version}_darwin_amd64.zip"
   sha256 '187594f54037e6261c116e04c57a1c8d9f113ded8d3b26e9b61440b6a53c93e5'
 
   def install
--- Formula/gobump.rb
+++ Formula/gobump.rb
@@ -4,7 +4,7 @@ class Gobump < Formula
   version 'v0.2.0'
   license 'MIT'
 
-  url "https://github.com/mattn/files/releases/download/#{version}/gobump_#{version}_darwin_amd64.zip"
+  url "#{homepage}/releases/download/#{version}/gobump_#{version}_darwin_amd64.zip"
   sha256 'ac837e8d7476b5c132d7ee3cea85d02d964e22050a3abbc2716177e56ae10be9'
 
   def install
