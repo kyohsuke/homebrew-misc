7a163a31e9765fb4bb96112e30cf5b18ebc03845
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2022-03-27 11:37:39 +0900

    Bump up difftastic

--- Formula/difftastic.rb
+++ Formula/difftastic.rb
@@ -1,11 +1,11 @@
 class Difftastic < Formula
   desc 'Difftastic is an experimental diff tool that compares files based on their syntax.'
   homepage 'https://github.com/wilfred/difftastic'
-  version '0.21.0'
+  version '0.24.0'
   license 'MIT'
 
   url "#{homepage}/archive/refs/tags/#{version}.tar.gz"
-  sha256 'b65b03b462fcd765c8b606c6d6bdf55d8b0df7844c2f112763a15a5e96511794'
+  sha256 '195bc08b36132b4fea90942243dece209573f6123bd8c60dabaad869e7441489'
 
   depends_on "rust" => :build
 
