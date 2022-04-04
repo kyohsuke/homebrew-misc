ffeefca3e5d2f61acceb48e186e9da26822bfd40
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2022-04-05 02:24:02 +0900

    Fix

--- Formula/gobump.rb
+++ Formula/gobump.rb
@@ -4,7 +4,6 @@ class Gobump < Formula
   url "https://github.com/x-motemen/gobump/releases/download/v0.2.0/gobump_v0.2.0_darwin_amd64.zip"
   version "0.2.0"
   sha256 "ac837e8d7476b5c132d7ee3cea85d02d964e22050a3abbc2716177e56ae10be9"
-  name "gobump"
   license "MIT"
 
   def install
