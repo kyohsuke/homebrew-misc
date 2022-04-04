a44c11bf488b32af9cbc6d3f5dac02dd24cfaa29
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2022-04-05 02:02:57 +0900

    fix

--- Formula/difftastic.rb
+++ Formula/difftastic.rb
@@ -1,7 +1,7 @@
 class Difftastic < Formula
   desc "Is an experimental diff tool that compares files based on their syntax"
   homepage "https://github.com/wilfred/difftastic"
-  url "#{homepage}/archive/refs/tags/#{version}.tar.gz"
+  url "https://github.com/wilfred/difftastic/archive/refs/tags/0.25.0.tar.gz"
   version "0.25.0"
   sha256 "f63ce86ab0b9a2b036b4c61d9601d7046dd79c91be0a0e5a9b3b2a4a7fa66eee"
   license "MIT"
--- Formula/files.rb
+++ Formula/files.rb
@@ -1,7 +1,7 @@
 class Files < Formula
   desc "Fast file find"
   homepage "https://github.com/mattn/files"
-  url "#{homepage}/releases/download/v#{version}/files_v#{version}_darwin_amd64.zip"
+  url "https://github.com/mattn/files/releases/download/v0.3.3/files_v0.3.3_darwin_amd64.zip"
   version "0.3.3"
   sha256 "187594f54037e6261c116e04c57a1c8d9f113ded8d3b26e9b61440b6a53c93e5"
   license "MIT"
