21a157c3ed46b29b502808d98586186b5c731674
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2024-12-24 14:28:55 +0900

    Add cooViewer

---
+++ Casks/cooviewer.rb
@@ -0,0 +1,11 @@
+cask "cooviewer" do
+  version "1.2b25"
+  sha256 "b085124c540ce6e7cf2f8dbf1c2a0dc7ae47133d9afbc73357ac98ea168cf09e"
+
+  url "https://github.com/coo-ona/cooViewer/releases/download#{version}/cooViewer.zip", verified: "github.com/"
+  name "cooViewer"
+  desc "simple comic viewer for Mac"
+  homepage "https://coo-ona.github.io/cooViewer/"
+
+  app "cooVewer.app", target: "/Applications/cooVewer.app"
+end
