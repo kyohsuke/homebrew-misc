36a8230038c839a3ee5d7e7c5fb8da1b8fca92a1
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2025-04-26 20:29:43 +0900

    Bump up shtp

--- Formula/sthp.rb
+++ Formula/sthp.rb
@@ -1,9 +1,9 @@
 class Sthp < Formula
   desc "Executable to convert SOCKS5 proxy into HTTP proxy"
   homepage "https://github.com/KaranGauswami/socks-to-http-proxy"
-  url "https://github.com/KaranGauswami/socks-to-http-proxy/archive/refs/tags/v#{version}.tar.gz"
-  version "0.4.0"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  url "https://github.com/KaranGauswami/socks-to-http-proxy/archive/refs/tags/#{version}.tar.gz"
+  version "0.5.0-beta2"
+  sha256 "f401b6f726e9148c70b15f53b44a2db43f97a2446c6a80d84529dff347d1bb68"
 
   head "https://github.com/KaranGauswami/socks-to-http-proxy.git", branch: "main"
 
